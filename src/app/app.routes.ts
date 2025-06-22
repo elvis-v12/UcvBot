e416d71a5142bb087e9cff6d60a126ad1ae76ed8
Luis24-creat <theluis390@gmail.com>
2025-06-21 20:42:17 -0500

    Primer commit - Proyecto Angular chatbot1.2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PerfilComponent } from './perfil/perfil';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'perfil', component: PerfilComponent},
+  { path: '', redirectTo: 'perfil', pathMatch: 'full' },
+  { path: '**', redirectTo: 'perfil' }
+
+];
---
+++ src/app/perfil/perfil.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil',
+  standalone: true,
+  imports: [],
+  templateUrl: './perfil.html',
+  styleUrl: './perfil.scss'
+})
+export class PerfilComponent  {
+
+}
