664ac011363df979b61342bc320c4314dfef5686
sthegfano236 <sthefanocamayo@gmail.com>
2025-06-24 01:12:11 -0500

    Agrega componente Dashboard con rutas actualizadas

--- frontend/admin-chatbot/src/app/app.routes.ts
+++ frontend/admin-chatbot/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ResetPasswordComponent } from './reset-password/reset-password.componen
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'reset-password', component: ResetPasswordComponent },
+  { path: 'dashboard', loadComponent: () => import('./Dashboard/dashboard.component').then(m => m.DashboardComponent) },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
 ];
